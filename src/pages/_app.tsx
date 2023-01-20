fa86532cad3b148235a9798aed558104d429846c
chidodev <chidev0104@gmail.com>
2023-01-20 11:37:53 -0800

    feat: config emotion cache and ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,61 @@
-import '@/styles/globals.css';
+import Head from 'next/head';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { CacheProvider } from '@emotion/react';
+import type { EmotionCache } from '@emotion/cache';
+
+// Component Imports
+import Layout from 'src/layouts';
+import ThemeComponent from 'src/theme/ThemeComponent';
+
+// context
+import {
+  SettingsConsumer,
+  SettingsProvider,
+} from 'src/context/settingsContext';
+
+import { createEmotionCache } from 'src/utils/create-emotion-cache';
+
+// Global css styles
+import '../../styles/globals.css';
+
+// extend App Props with Emotion
+type ExtendedAppProps = AppProps & {
+  Component: NextPage;
+  emotionCache: EmotionCache;
+};
+
+const clientSideEmotionCache = createEmotionCache();
+
+const App = (props: ExtendedAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  // Variables
+  const getLayout = (page: React.ReactNode) => <Layout>{page}</Layout>;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Youthfully Test</title>
+        <meta name="description" content="youthfull test" />
+        <meta name="keywords" content="NextJS, SSR" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <SettingsProvider>
+        <SettingsConsumer>
+          {({ settings }) => {
+            return (
+              <ThemeComponent settings={settings}>
+                {getLayout(<Component {...pageProps} />)}
+              </ThemeComponent>
+            );
+          }}
+        </SettingsConsumer>
+      </SettingsProvider>
+    </CacheProvider>
+  );
+};
+
+export default App;
