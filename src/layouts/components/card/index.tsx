9c21eadc03c39ead092727495ae0dea878cabc7a
chidodev <chidev0104@gmail.com>
2023-01-21 09:58:01 +0800

    feat: enlarge image with description

--- src/layouts/components/card/index.tsx
+++ src/layouts/components/card/index.tsx
@@ -1,36 +1,47 @@
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import ImageEnlarger from '../image';
 
 import { ImageDataType, ImageType } from '@/src/utils/types';
 
 const ImageCard = (props: ImageDataType) => {
-  const { title, description, images_count, link, ups, images } = props;
+  const { title, description, images_count, score, ups, downs, images } = props;
 
   const isImgUrl = (url: string) => {
     return /\.(jpg|jpeg|png|webp|avif|gif)$/.test(url);
   };
 
+  const [zoomed, setZoomed] = useState(false);
   return (
     <>
       {images_count && isImgUrl(images[0].link) && (
         <Grid item xs={12} md={4}>
           <Card>
             <CardContent>
-              <CardMedia
-                component="img"
-                alt=""
-                height="140"
-                image={images[0].link}
-                sx={{
-                  borderRadius: '7px',
-                }}
-              />
-              <Typography mt={5} variant="body1">
-                {images[0].description || 'No descriptin'}
-              </Typography>
+              <Box>
+                <ImageEnlarger
+                  title={title}
+                  description={description}
+                  ups={ups}
+                  downs={downs}
+                  score={score}
+                  style={{ width: '100%' }}
+                  zoomed={zoomed}
+                  src={images[0].link}
+                  onClick={() => setZoomed(true)}
+                  onRequestClose={() => setZoomed(false)}
+                />
+              </Box>
+              <Box>
+                <Typography mt={5} variant="body1">
+                  {images[0].description || 'No descriptin'}
+                </Typography>
+              </Box>
             </CardContent>
           </Card>
         </Grid>
